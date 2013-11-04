518634c8d49427c0cd40ee864a46bcfbcd2a25fe
Amir Kadivar <amir.h.kadivar@gmail.com>
2013-11-04 11:27:39 -0500

    print logs in client_connect

--- client-connect.sh
+++ client-connect.sh
@@ -5,10 +5,12 @@
 # url must include HTTP authorization credentials if necessary
 # http://[user]:[pass]@[fqdn]/id_rsa.pub
 url=$1
-backuppc_key=$( curl -s $url )
-
+[[ -z $url ]] && echo 'you must specify a URL' && exit 1
+echo 'downloading public key from $url/id_rsa.pub ...'
+backuppc_key=$( curl -s $url/id_rsa.pub )
 [[ ! -d /root/.ssh ]] && mkdir /root/.ssh
 
 keys_file='/root/.ssh/authorized_keys'
 touch $keys_file
+echo 'copying key to authorized_keys file ...'
 grep -q "$backuppc_key" $keys_file || echo $backuppc_key >> $keys_file
